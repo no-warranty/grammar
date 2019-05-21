3e38be9f302a7f38152764420e0045501e583b2c
Richard B. Wagner <breckwagner@gmail.com>
2019-05-21 17:46:09 +0000

    Update index.mjs

--- packages/css/src/index.mjs
+++ packages/css/src/index.mjs
@@ -1,10 +1,20 @@
-export default class {
+export const css = class {
   /**
    * @param {Iterable.<string>} callSite
    * @param {Iterable.<string>} substitutions
    * @return {CSSStyleSheet}
    */
-  static template(callSite, ...substitutions) {
-    return new CSSStyleSheet().replace(callSite.reduce((arr, v, i) => arr.concat(v, substitutions[i]), []))
+   
+  constructor () {
+    return 'static method has been called.';
   }
+  static template(callSite, ...substitutions) {
+    return (new CSSStyleSheet()).replace(callSite.reduce((arr, v, i) => arr.concat(v, substitutions[i]), []))
+  }
+}
+
+export default function() {
+  return (new.target) 
+  ? CSS 
+  : CSS.template;
 }
